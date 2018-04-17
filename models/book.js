3aaa1de9eaac583ec40cd83c608f448d5ca40a15
Justin Brown <justin.v.brown@gmail.com>
2018-04-17 17:32:52 -0500

    Edited server.js Commented out book array and edited 'get all books' fucnction.

--- models/book.js
+++ models/book.js
@@ -1,14 +1,13 @@
 // book.js
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var BookSchema = new Schema({
-     title: String,
-     author: String,
-     image: String,
-     releaseDate: String,
- });
+var BookSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  image: String,
+  release_date: String
+});
 
- var Book = mongoose.model('Book', BookSchema);
+var Book = mongoose.model('Book', BookSchema);
 
- module.exports = Book;
+module.exports = Book;
